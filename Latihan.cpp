f62c23e856e644982f59b8cccf03e5e2badeebef
FathiyyaJasmine <112569446+FathiyyaJasmine@users.noreply.github.com>
2022-09-09 11:08:23 +0700

    Add files via upload

---
+++ Latihan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+	float phi = 3.14, Luas ;
+	int r;
+	
+	cout<<"Masukan jari jari lingkaran : ";
+	cin>>r;
+	
+	Luas = phi*r*r;
+	cout<<"Luas lingkaran adalah : "<< Luas;
+	
+	return 0;
+}
